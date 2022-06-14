b5da077ea7e23e19ff91f576864c1bcbc6b12036
Ondrej Demeter <ondrej.demeter5@gmail.com>
2022-06-14 15:51:59 +0100

    Codewars challenges

--- codewars.js
+++ codewars.js
@@ -64,6 +64,7 @@ function removeChar(str){
 Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
 
 Examples:
+DESCRIPTION:
 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
@@ -77,3 +78,27 @@ function isDivisible(n, x, y) {
     return false;
   }
 };
+
+/*
+DESCRIPTION:
+Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+setAlarm(true, true) -> false
+setAlarm(false, true) -> false
+setAlarm(false, false) -> false
+setAlarm(true, false) -> true
+*/
+
+function setAlarm(employed, vacation){
+  if(employed == true && vacation == false) {
+    return true;
+  } else if(employed == false && vacation == false) {
+    return false;
+  } else if(employed == false && vacation == true) {
+    return false;
+  } else {
+    return false;
+  }
+}
